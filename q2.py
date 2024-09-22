3a5e3ca9c943456a3b63dfece582f0a75e71e2b6
Max Moir <maxwmoir@gmail.com>
2024-09-22 15:23:25 +1200

    Update q2.py

--- q2.py
+++ q2.py
@@ -4,12 +4,10 @@ import numpy.linalg as la
 import matplotlib.pyplot as plt
 import time
 
-
-sizes = list(range(2, 5000, 100)) # For assign should be 5k but takes ages to run
+sizes = list(range(2, 5000, 100)) 
 sizes.append(5000)
 times = []
 theo = []
-
 
 for n in sizes:
     A = np.random.rand(n, n)
@@ -19,7 +17,6 @@ for n in sizes:
     t1 = time.time()
     times.append(t1 - t0)
     theo.append((59.7 * 10**-12) * (2/3 * n**3 + 2*n**2))
-
 
 plt.plot(sizes, times, 'b')
 plt.plot(sizes, theo, 'go')
